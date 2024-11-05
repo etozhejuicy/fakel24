c2ab8597153b53f926bb8102fb5f66b33830c8fd
Kind Samurai <57537080+etozhejuicy@users.noreply.github.com>
2024-11-05 17:46:38 +0300

    update

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,10 +1,10 @@
 // heroCard init
-const heroCard = document.querySelector("[data-card-hero]");
-const techCard = document.querySelector("[data-card-tech]");
+const heroCard = document.querySelector('[data-card-hero]');
+const techCard = document.querySelector('[data-card-tech]');
 
 // swiper hero init
-if (document.getElementsByClassName("section-hero-swiper").length) {
-  var swiperHero = new Swiper(".swiper-hero", {
+if (document.getElementsByClassName('section-hero-swiper').length) {
+  var swiperHero = new Swiper('.swiper-hero', {
     slidesPerView: 1,
     spaceBetween: 20,
     speed: 400,
@@ -14,7 +14,7 @@ if (document.getElementsByClassName("section-hero-swiper").length) {
       enabled: false,
     },
     pagination: {
-      el: ".swiper-hero [swiper-pagination]",
+      el: '.swiper-hero [swiper-pagination]',
       clickable: true,
     },
     navigation: {
@@ -28,29 +28,29 @@ if (document.getElementsByClassName("section-hero-swiper").length) {
 
         activeSlide = activeSlide + 1;
 
-        heroCard.classList.add("fadeEffect");
+        heroCard.classList.add('fadeEffect');
 
         setTimeout(() => {
-          heroCard.classList.remove("fadeEffect");
+          heroCard.classList.remove('fadeEffect');
         }, 500);
 
         if (activeSlide == 1) {
-          heroCard.querySelector(".card-title").innerText =
-            "Профессиональная гигиена всей полости рта";
-          heroCard.querySelector(".card-body p").innerHTML =
-            "Скидка первичным пациентам 900 руб!";
+          heroCard.querySelector('.card-title').innerText =
+            'Профессиональная гигиена всей полости рта';
+          heroCard.querySelector('.card-body p').innerHTML =
+            'Скидка первичным пациентам 900 руб!';
         } else if (activeSlide == 2) {
-          heroCard.querySelector(".card-title").innerText = "Имплантация ";
-          heroCard.querySelector(".card-body p").innerHTML =
-            "Установка Имплантатов SuperLine по выгодной цене за 24000 руб!";
+          heroCard.querySelector('.card-title').innerText = 'Имплантация ';
+          heroCard.querySelector('.card-body p').innerHTML =
+            'Установка Имплантатов SuperLine по выгодной цене за 24000 руб!';
         } else if (activeSlide == 3) {
-          heroCard.querySelector(".card-title").innerText = "Плазмолифтинг";
-          heroCard.querySelector(".card-body p").innerHTML =
-            "Здоровые дёсна - залог красивой улыбки";
+          heroCard.querySelector('.card-title').innerText = 'Плазмолифтинг';
+          heroCard.querySelector('.card-body p').innerHTML =
+            'Здоровые дёсна - залог красивой улыбки';
         } else if (activeSlide == 4) {
-          heroCard.querySelector(".card-title").innerText = "ПРОТЕЗИРОВАНИЕ";
-          heroCard.querySelector(".card-body p").innerHTML =
-            "Изготовление безметалловых коронок по цене от 16500 руб!";
+          heroCard.querySelector('.card-title').innerText = 'ПРОТЕЗИРОВАНИЕ';
+          heroCard.querySelector('.card-body p').innerHTML =
+            'Изготовление безметалловых коронок по цене от 16500 руб!';
         }
       },
     },
@@ -64,9 +64,9 @@ if (document.getElementsByClassName("section-hero-swiper").length) {
 }
 
 // swiper doctor init
-if (document.getElementsByClassName("section-doctors").length) {
-  var swiperDoctors = new Swiper(".swiper-doctors", {
-    slidesPerView: "auto",
+if (document.getElementsByClassName('section-doctors').length) {
+  var swiperDoctors = new Swiper('.swiper-doctors', {
+    slidesPerView: 'auto',
     spaceBetween: 20,
     speed: 400,
     breakpoints: {
@@ -84,7 +84,7 @@ if (document.getElementsByClassName("section-doctors").length) {
       enabled: false,
     },
     pagination: {
-      el: ".swiper-doctors [swiper-pagination]",
+      el: '.swiper-doctors [swiper-pagination]',
       clickable: true,
     },
     navigation: {
@@ -102,8 +102,8 @@ if (document.getElementsByClassName("section-doctors").length) {
 }
 
 // swiper technologies
-if (document.getElementsByClassName("section-technologies").length) {
-  var swiperTechnologies = new Swiper(".swiper-technologies", {
+if (document.getElementsByClassName('section-technologies').length) {
+  var swiperTechnologies = new Swiper('.swiper-technologies', {
     slidesPerView: 1,
     spaceBetween: 20,
     speed: 400,
@@ -113,7 +113,7 @@ if (document.getElementsByClassName("section-technologies").length) {
       enabled: false,
     },
     pagination: {
-      el: ".swiper-technologies [swiper-pagination]",
+      el: '.swiper-technologies [swiper-pagination]',
       clickable: true,
     },
     navigation: {
@@ -127,25 +127,25 @@ if (document.getElementsByClassName("section-technologies").length) {
 
         activeSlide = activeSlide + 1;
 
-        techCard.classList.add("fadeEffect");
+        techCard.classList.add('fadeEffect');
 
         setTimeout(() => {
-          techCard.classList.remove("fadeEffect");
+          techCard.classList.remove('fadeEffect');
         }, 500);
 
         if (activeSlide == 1) {
-          techCard.querySelector(".card-title").innerText = "ФИЗИОДИСПЕНСЕР";
-          techCard.querySelector("p").innerText =
-            "В клинике проводятся имплантация зубов с последующим протезированием и сложные зубо-сохраняющие операции с использованием проверенного и качественного оборудования фирмы NSK (физиодиспенсора). Лечение корневых каналов зубов проводится при помощи машинных, а не ручных инструментов под контролем апекслокаторов и рентгендиагностики, что значительно сокращает время проведения необходимых процедур, повышает качество и способствует долгосрочности результата.";
+          techCard.querySelector('.card-title').innerText = 'ФИЗИОДИСПЕНСЕР';
+          techCard.querySelector('p').innerText =
+            'В клинике проводятся имплантация зубов с последующим протезированием и сложные зубо-сохраняющие операции с использованием проверенного и качественного оборудования фирмы NSK (физиодиспенсора). Лечение корневых каналов зубов проводится при помощи машинных, а не ручных инструментов под контролем апекслокаторов и рентгендиагностики, что значительно сокращает время проведения необходимых процедур, повышает качество и способствует долгосрочности результата.';
         } else if (activeSlide == 2) {
-          techCard.querySelector(".card-title").innerText =
-            "Оборудованный кабинет ";
-          techCard.querySelector("p").innerText =
-            "Каждый кабинет оборудован по самым современным медицинским стандартам для удобства и комфорта пациента. Специалисты используют только сертифицированные и качественные светоотверждаемые пломбировочные материалы для лечения.";
+          techCard.querySelector('.card-title').innerText =
+            'Оборудованный кабинет ';
+          techCard.querySelector('p').innerText =
+            'Каждый кабинет оборудован по самым современным медицинским стандартам для удобства и комфорта пациента. Специалисты используют только сертифицированные и качественные светоотверждаемые пломбировочные материалы для лечения.';
         } else if (activeSlide == 3) {
-          techCard.querySelector(".card-title").innerText = "СИСТЕМА AIR FLOW";
-          techCard.querySelector("p").innerText =
-            "Для повышения эффективности проводимых манипуляций и отбеливание в нашей клинике осуществляется в «четыре руки» - с непосредственным участием ассистента врача стоматолога. Профессиональная гигиена полости рта – самая комфортная и эффективная процедура для профилактики заболеваний зубов и десен, рекомендуемая в нашей клинике всем пациентам. Для повышения качества её проведения обязательным является использование системы Air Flow (дословно - воздушного потока).";
+          techCard.querySelector('.card-title').innerText = 'СИСТЕМА AIR FLOW';
+          techCard.querySelector('p').innerText =
+            'Для повышения эффективности проводимых манипуляций и отбеливание в нашей клинике осуществляется в «четыре руки» - с непосредственным участием ассистента врача стоматолога. Профессиональная гигиена полости рта – самая комфортная и эффективная процедура для профилактики заболеваний зубов и десен, рекомендуемая в нашей клинике всем пациентам. Для повышения качества её проведения обязательным является использование системы Air Flow (дословно - воздушного потока).';
         }
       },
     },
@@ -159,11 +159,11 @@ if (document.getElementsByClassName("section-technologies").length) {
 }
 
 // swiper smiles init
-if (document.getElementsByClassName("section-smile").length) {
-  var swiperSmiles = new Swiper(".swiper-smiles", {
-    slidesPerView: "auto",
+if (document.getElementsByClassName('section-smile').length) {
+  var swiperSmiles = new Swiper('.swiper-smiles', {
+    slidesPerView: 'auto',
     spaceBetween: 20,
-    noSwipingSelector: ".swiper-smiles .smile-images",
+    noSwipingSelector: '.swiper-smiles .smile-images',
     breakpoints: {
       0: {
         centeredSlides: true,
@@ -179,7 +179,7 @@ if (document.getElementsByClassName("section-smile").length) {
       enabled: false,
     },
     pagination: {
-      el: ".swiper-smiles [swiper-pagination]",
+      el: '.swiper-smiles [swiper-pagination]',
       clickable: true,
     },
     navigation: {
@@ -197,9 +197,9 @@ if (document.getElementsByClassName("section-smile").length) {
 }
 
 // swiper reviews init
-if (document.getElementsByClassName("section-reviews").length) {
-  var swiperReviews = new Swiper(".swiper-reviews", {
-    slidesPerView: "auto",
+if (document.getElementsByClassName('section-reviews').length) {
+  var swiperReviews = new Swiper('.swiper-reviews', {
+    slidesPerView: 'auto',
     spaceBetween: 20,
     breakpoints: {
       0: {
@@ -216,7 +216,7 @@ if (document.getElementsByClassName("section-reviews").length) {
       enabled: false,
     },
     pagination: {
-      el: ".swiper-reviews [swiper-pagination]",
+      el: '.swiper-reviews [swiper-pagination]',
       clickable: true,
     },
     navigation: {
@@ -234,15 +234,15 @@ if (document.getElementsByClassName("section-reviews").length) {
 }
 
 // swiper result init
-if (document.getElementsByClassName("section-result").length) {
-  var swiperResult = new Swiper(".swiper-result", {
+if (document.getElementsByClassName('section-result').length) {
+  var swiperResult = new Swiper('.swiper-result', {
     breakpoints: {
       0: {
         slidesPerView: 2,
         spaceBetween: 4,
         grid: {
           rows: 1,
-          fill: "row",
+          fill: 'row',
         },
       },
       720: {
@@ -250,7 +250,7 @@ if (document.getElementsByClassName("section-result").length) {
         spaceBetween: 12,
         grid: {
           rows: 3,
-          fill: "row",
+          fill: 'row',
         },
       },
       992: {
@@ -258,7 +258,7 @@ if (document.getElementsByClassName("section-result").length) {
         spaceBetween: 12,
         grid: {
           rows: 2,
-          fill: "row",
+          fill: 'row',
         },
       },
     },
@@ -266,7 +266,7 @@ if (document.getElementsByClassName("section-result").length) {
       enabled: false,
     },
     pagination: {
-      el: ".swiper-result [swiper-pagination]",
+      el: '.swiper-result [swiper-pagination]',
       clickable: true,
     },
     navigation: {
@@ -284,83 +284,83 @@ if (document.getElementsByClassName("section-result").length) {
 }
 
 // header script
-const logo = document.querySelector(".header-logo");
-const mainMenu = document.querySelector(".menu");
-const headerTop = document.querySelector(".header-top");
-const header = document.querySelector("header");
-const headerModalBtn = document.querySelector(".header-modal-btn");
+const logo = document.querySelector('.header-logo');
+const mainMenu = document.querySelector('.menu');
+const headerTop = document.querySelector('.header-top');
+const header = document.querySelector('header');
+const headerModalBtn = document.querySelector('.header-modal-btn');
 
-window.addEventListener("scroll", setFixedHeader);
-window.addEventListener("resize", setFixedHeader);
-window.addEventListener("DOMContentLoaded", setFixedHeader);
+window.addEventListener('scroll', setFixedHeader);
+window.addEventListener('resize', setFixedHeader);
+window.addEventListener('DOMContentLoaded', setFixedHeader);
 
 function setFixedHeader() {
   if (window.innerWidth >= 1200) {
     if (window.scrollY >= 1) {
-      logo.classList.add("scroll");
-      mainMenu.classList.add("scroll");
-      headerModalBtn.classList.add("scroll");
+      logo.classList.add('scroll');
+      mainMenu.classList.add('scroll');
+      headerModalBtn.classList.add('scroll');
     } else {
-      logo.classList.remove("scroll");
-      mainMenu.classList.remove("scroll");
-      headerModalBtn.classList.remove("scroll");
+      logo.classList.remove('scroll');
+      mainMenu.classList.remove('scroll');
+      headerModalBtn.classList.remove('scroll');
     }
   } else {
-    logo.classList.remove("scroll");
-    mainMenu.classList.remove("scroll");
-    headerModalBtn.classList.remove("scroll");
+    logo.classList.remove('scroll');
+    mainMenu.classList.remove('scroll');
+    headerModalBtn.classList.remove('scroll');
   }
 }
 
 // fix header script classList before load
-window.addEventListener("DOMContentLoaded", () => {
-  logo.classList.remove("scroll");
-  mainMenu.classList.remove("scroll");
-  headerModalBtn.classList.remove("scroll");
+window.addEventListener('DOMContentLoaded', () => {
+  logo.classList.remove('scroll');
+  mainMenu.classList.remove('scroll');
+  headerModalBtn.classList.remove('scroll');
 });
 
 // for dropdown menu icon
-const dropdownItem = document.querySelectorAll(".menu-dropdown");
+const dropdownItem = document.querySelectorAll('.menu-dropdown');
 
 function dropdownMenu() {
   if (window.innerWidth >= 1200) {
     dropdownItem.forEach((item) => {
-      item.addEventListener("mouseenter", () => {
-        if (item.classList.contains("opened")) {
-          item.classList.remove("opened");
+      item.addEventListener('mouseenter', () => {
+        if (item.classList.contains('opened')) {
+          item.classList.remove('opened');
         } else {
-          item.classList.add("opened");
+          item.classList.add('opened');
         }
       });
-      item.addEventListener("mouseleave", () => {
-        if (item.classList.contains("opened")) {
-          item.classList.remove("opened");
+      item.addEventListener('mouseleave', () => {
+        if (item.classList.contains('opened')) {
+          item.classList.remove('opened');
         } else {
-          item.classList.add("opened");
+          item.classList.add('opened');
         }
       });
     });
   }
 }
 
-window.addEventListener("DOMContentLoaded", dropdownMenu);
-window.addEventListener("resize", dropdownMenu);
+window.addEventListener('DOMContentLoaded', dropdownMenu);
+window.addEventListener('resize', dropdownMenu);
 
 // mobile-menu
-const menuOpenElems = document.querySelectorAll("[data-menu-open]");
-const menuCloseElems = document.querySelectorAll("[data-menu-close]");
+const menuOpenElems = document.querySelectorAll('[data-menu-open]');
+const menuCloseElems = document.querySelectorAll('[data-menu-close]');
 
 // mobile-menu
 function mobileMenu() {
   if (window.innerWidth <= 1200) {
     menuOpenElems.forEach((menuOpenElem) => {
-      menuOpenElem.addEventListener("click", () => {
+      menuOpenElem.addEventListener('click', () => {
         openMenu();
       });
     });
 
     menuCloseElems.forEach((menuCloseElem) => {
-      menuCloseElem.addEventListener("click", () => {
+      menuCloseElem.addEventListener('click', () => {
         closeMenu();
       });
     });
@@ -368,50 +368,50 @@ function mobileMenu() {
 }
 
 function openMenu() {
-  mainMenu.classList.add("menu-show");
-  document.documentElement.classList.add("menu-open");
+  mainMenu.classList.add('menu-show');
+  document.documentElement.classList.add('menu-open');
 }
 
 function closeMenu() {
-  mainMenu.classList.remove("menu-show");
-  document.documentElement.classList.remove("menu-open");
+  mainMenu.classList.remove('menu-show');
+  document.documentElement.classList.remove('menu-open');
 }
 
-window.addEventListener("DOMContentLoaded", mobileMenu);
-window.addEventListener("scroll", mobileMenu);
-window.addEventListener("resize", mobileMenu);
+window.addEventListener('DOMContentLoaded', mobileMenu);
+window.addEventListener('scroll', mobileMenu);
+window.addEventListener('resize', mobileMenu);
 
 // modals logic
-const openModalButtons = document.querySelectorAll("[data-modal-open]");
-const closeButtons = document.querySelectorAll("[btn-close-modal]");
+const openModalButtons = document.querySelectorAll('[data-modal-open]');
+const closeButtons = document.querySelectorAll('[btn-close-modal]');
 
 openModalButtons.forEach((button) => {
-  button.addEventListener("click", function (e) {
-    const modalKey = e.currentTarget.getAttribute("data-modal-open");
+  button.addEventListener('click', function (e) {
+    const modalKey = e.currentTarget.getAttribute('data-modal-open');
     const modal = document.querySelector('[data-modal-id="' + modalKey + '"]');
     if (modal) {
-      modal.classList.add("show");
-      document.documentElement.classList.add("modal-open");
+      modal.classList.add('show');
+      document.documentElement.classList.add('modal-open');
     }
   });
 });
 
 closeButtons.forEach((button) => {
-  button.addEventListener("click", function () {
-    const modal = button.closest(".modal");
+  button.addEventListener('click', function () {
+    const modal = button.closest('.modal');
     if (modal) {
-      modal.classList.remove("show");
-      document.documentElement.classList.remove("modal-open");
+      modal.classList.remove('show');
+      document.documentElement.classList.remove('modal-open');
     }
   });
 });
 
-window.addEventListener("click", function (event) {
-  const modals = document.querySelectorAll(".modal");
+window.addEventListener('click', function (event) {
+  const modals = document.querySelectorAll('.modal');
   modals.forEach((modal) => {
     if (event.target === modal) {
-      modal.classList.remove("show");
-      document.documentElement.classList.remove("modal-open");
+      modal.classList.remove('show');
+      document.documentElement.classList.remove('modal-open');
     }
   });
 });
