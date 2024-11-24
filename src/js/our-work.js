f02f4887a86c10f720f42dc2e52304a03353b6e7
serj <zenitovic@gmail.com>
2024-11-24 19:08:02 +0300

    update specialist & some blocks for other pages

--- src/js/our-work.js
+++ src/js/our-work.js
@@ -10,52 +10,52 @@ window.addEventListener('resize', setFixedHeader);
 window.addEventListener('DOMContentLoaded', setFixedHeader);
 
 function setFixedHeader() {
-  if (window.innerWidth >= 1200) {
-    if (window.scrollY >= 1) {
-      logo.classList.add('scroll');
-      mainMenu.classList.add('scroll');
-      headerModalBtn.classList.add('scroll');
+    if (window.innerWidth >= 1200) {
+        if (window.scrollY >= 1) {
+            logo.classList.add('scroll');
+            mainMenu.classList.add('scroll');
+            headerModalBtn.classList.add('scroll');
+        } else {
+            logo.classList.remove('scroll');
+            mainMenu.classList.remove('scroll');
+            headerModalBtn.classList.remove('scroll');
+        }
     } else {
-      logo.classList.remove('scroll');
-      mainMenu.classList.remove('scroll');
-      headerModalBtn.classList.remove('scroll');
+        logo.classList.remove('scroll');
+        mainMenu.classList.remove('scroll');
+        headerModalBtn.classList.remove('scroll');
     }
-  } else {
-    logo.classList.remove('scroll');
-    mainMenu.classList.remove('scroll');
-    headerModalBtn.classList.remove('scroll');
-  }
 }
 
 // fix header script classList before load
 window.addEventListener('DOMContentLoaded', () => {
-  logo.classList.remove('scroll');
-  mainMenu.classList.remove('scroll');
-  headerModalBtn.classList.remove('scroll');
+    logo.classList.remove('scroll');
+    mainMenu.classList.remove('scroll');
+    headerModalBtn.classList.remove('scroll');
 });
 
 // for dropdown menu icon
 const dropdownItem = document.querySelectorAll('.menu-dropdown');
 
 function dropdownMenu() {
-  if (window.innerWidth >= 1200) {
-    dropdownItem.forEach((item) => {
-      item.addEventListener('mouseenter', () => {
-        if (item.classList.contains('opened')) {
-          item.classList.remove('opened');
-        } else {
-          item.classList.add('opened');
-        }
-      });
-      item.addEventListener('mouseleave', () => {
-        if (item.classList.contains('opened')) {
-          item.classList.remove('opened');
-        } else {
-          item.classList.add('opened');
-        }
-      });
-    });
-  }
+    if (window.innerWidth >= 1200) {
+        dropdownItem.forEach((item) => {
+            item.addEventListener('mouseenter', () => {
+                if (item.classList.contains('opened')) {
+                    item.classList.remove('opened');
+                } else {
+                    item.classList.add('opened');
+                }
+            });
+            item.addEventListener('mouseleave', () => {
+                if (item.classList.contains('opened')) {
+                    item.classList.remove('opened');
+                } else {
+                    item.classList.add('opened');
+                }
+            });
+        });
+    }
 }
 
 window.addEventListener('DOMContentLoaded', dropdownMenu);
@@ -67,29 +67,29 @@ const menuCloseElems = document.querySelectorAll('[data-menu-close]');
 
 // mobile-menu
 function mobileMenu() {
-  if (window.innerWidth <= 1200) {
-    menuOpenElems.forEach((menuOpenElem) => {
-      menuOpenElem.addEventListener('click', () => {
-        openMenu();
-      });
-    });
+    if (window.innerWidth <= 1200) {
+        menuOpenElems.forEach((menuOpenElem) => {
+            menuOpenElem.addEventListener('click', () => {
+                openMenu();
+            });
+        });
 
-    menuCloseElems.forEach((menuCloseElem) => {
-      menuCloseElem.addEventListener('click', () => {
-        closeMenu();
-      });
-    });
-  }
+        menuCloseElems.forEach((menuCloseElem) => {
+            menuCloseElem.addEventListener('click', () => {
+                closeMenu();
+            });
+        });
+    }
 }
 
 function openMenu() {
-  mainMenu.classList.add('menu-show');
-  document.documentElement.classList.add('menu-open');
+    mainMenu.classList.add('menu-show');
+    document.documentElement.classList.add('menu-open');
 }
 
 function closeMenu() {
-  mainMenu.classList.remove('menu-show');
-  document.documentElement.classList.remove('menu-open');
+    mainMenu.classList.remove('menu-show');
+    document.documentElement.classList.remove('menu-open');
 }
 
 window.addEventListener('DOMContentLoaded', mobileMenu);
@@ -101,11 +101,11 @@ const serviceButton = document.querySelectorAll('[data-appointment-type]');
 
 // предотвращает переход по ссылке для всех сервисных карточек с кнопкой
 serviceButton.forEach((button) => {
-  button.addEventListener('click', (e) => {
-    e.stopPropagation();
-    e.preventDefault();
-    console.log('Кнопка нажата, переход по ссылке предотвращен.');
-  });
+    button.addEventListener('click', (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        console.log('Кнопка нажата, переход по ссылке предотвращен.');
+    });
 });
 
 // modals logic
@@ -113,34 +113,34 @@ const openModalButtons = document.querySelectorAll('[data-modal-open]');
 const closeButtons = document.querySelectorAll('[btn-close-modal]');
 
 openModalButtons.forEach((button) => {
-  button.addEventListener('click', function (e) {
-    const modalKey = e.currentTarget.getAttribute('data-modal-open');
-    const modal = document.querySelector('[data-modal-id="' + modalKey + '"]');
-    if (modal) {
-      modal.classList.add('show');
-      document.documentElement.classList.add('modal-open');
-    }
-  });
+    button.addEventListener('click', function(e) {
+        const modalKey = e.currentTarget.getAttribute('data-modal-open');
+        const modal = document.querySelector('[data-modal-id="' + modalKey + '"]');
+        if (modal) {
+            modal.classList.add('show');
+            document.documentElement.classList.add('modal-open');
+        }
+    });
 });
 
 closeButtons.forEach((button) => {
-  button.addEventListener('click', function () {
-    const modal = button.closest('.modal');
-    if (modal) {
-      modal.classList.remove('show');
-      document.documentElement.classList.remove('modal-open');
-    }
-  });
+    button.addEventListener('click', function() {
+        const modal = button.closest('.modal');
+        if (modal) {
+            modal.classList.remove('show');
+            document.documentElement.classList.remove('modal-open');
+        }
+    });
 });
 
-window.addEventListener('click', function (event) {
-  const modals = document.querySelectorAll('.modal');
-  modals.forEach((modal) => {
-    if (event.target === modal) {
-      modal.classList.remove('show');
-      document.documentElement.classList.remove('modal-open');
-    }
-  });
+window.addEventListener('click', function(event) {
+    const modals = document.querySelectorAll('.modal');
+    modals.forEach((modal) => {
+        if (event.target === modal) {
+            modal.classList.remove('show');
+            document.documentElement.classList.remove('modal-open');
+        }
+    });
 });
 
 // datepicker
@@ -150,77 +150,41 @@ currentDate.setHours(0, 0, 0, 0);
 const datepickers = document.querySelectorAll('[data-input-type="datepicker"]');
 
 datepickers.forEach((picker) => {
-  new Datepicker(picker, {
-    i18n: {
-      months: [
-        'Январь',
-        'Февраль',
-        'Март',
-        'Апрель',
-        'Май',
-        'Июнь',
-        'Июль',
-        'Август',
-        'Сентябрь',
-        'Октябрь',
-        'Ноябрь',
-        'Декабрь',
-      ],
-      weekdays: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
-    },
-    weekStart: 1,
-    min: currentDate,
-  });
+    new Datepicker(picker, {
+        i18n: {
+            months: [
+                'Январь',
+                'Февраль',
+                'Март',
+                'Апрель',
+                'Май',
+                'Июнь',
+                'Июль',
+                'Август',
+                'Сентябрь',
+                'Октябрь',
+                'Ноябрь',
+                'Декабрь',
+            ],
+            weekdays: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
+        },
+        weekStart: 1,
+        min: currentDate,
+    });
 });
 
 // map logic
 document.getElementById('map-preview').addEventListener('click', (e) => {
-  e.preventDefault();
-  // Скрыть скриншот и показать контейнер для карты
-  document.getElementById('map').classList.remove('preview');
-  document.getElementById('map-preview').style.display = 'none';
-  document.getElementById('map-source').style.display = 'block';
+    e.preventDefault();
+    // Скрыть скриншот и показать контейнер для карты
+    document.getElementById('map').classList.remove('preview');
+    document.getElementById('map-preview').style.display = 'none';
+    document.getElementById('map-source').style.display = 'block';
 
-  // Подгрузка скрипта карты
-  const script = document.createElement('script');
-  script.src =
-    'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Ac96fd1d6812b0862b53f34e6337afe3a3353a9c02a4a2d3592090f83a5e4040d&amp;width=100%25&amp;height=350&amp;lang=ru_RU&amp;scroll=disable';
-  script.defer = true;
-  document.getElementById('map-source').appendChild(script);
+    // Подгрузка скрипта карты
+    const script = document.createElement('script');
+    script.src =
+        'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Ac96fd1d6812b0862b53f34e6337afe3a3353a9c02a4a2d3592090f83a5e4040d&amp;width=100%25&amp;height=350&amp;lang=ru_RU&amp;scroll=disable';
+    script.defer = true;
+    document.getElementById('map-source').appendChild(script);
 });
-
-// swiper smiles init
-if (document.getElementsByClassName('section-smile').length) {
-  var swiperSmiles = new Swiper('.swiper-smiles', {
-    slidesPerView: 'auto',
-    spaceBetween: 20,
-    noSwipingSelector: '.swiper-smiles .smile-images',
-    breakpoints: {
-      0: {
-        centeredSlides: true,
-      },
-      1200: {
-        slidesPerView: 'auto',
-        centeredSlides: false,
-      },
-    },
-    scrollbar: {
-      enabled: false,
-    },
-    pagination: {
-      el: '.swiper-smiles [swiper-pagination]',
-      clickable: true,
-    },
-    navigation: {
-      enabled: false,
-    },
-    allowTouchMove: true,
-    grabCursor: true,
-  });
-
-  setTimeout(function () {
-    if (swiperSmiles.update) {
-      swiperSmiles.update();
-    }
-  }, 500);
-}
